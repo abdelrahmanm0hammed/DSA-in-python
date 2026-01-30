d33dc0469bc2ce684b3d9c37038afecb30e78854
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-01-30 21:20:47 +0200

    solution in plain english

--- locate_card.py
+++ locate_card.py
@@ -1,2 +1,9 @@
+#solution in plain english
+#1-create a variable position with the value 0
+#2-check whether the number at index position in card equals query
+#3- if it does, position is the answer and can be returned from the function
+#4-if not, increment the value of position by 1, and repeat steps 2 to 4 till we reach the last position
+#5- if the number was not found, return -1
+
 def locate_card(cards, query):
     pass
