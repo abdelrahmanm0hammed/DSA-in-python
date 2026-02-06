eeb368dbe1692c6eb3c9e0a40d123112b959f485
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-06 19:01:45 +0200

    solution using binary search

--- linear_and_binary_trees.py
+++ linear_and_binary_trees.py
@@ -7,8 +7,124 @@
 #2-Find the information of a user,given their username
 #3-Update the profile information of a user, given their username
 #4-List all users of the platform, sorted by username
+# You can assume that usernames are unique
+
+
+#Here's a systematic strategy we'll apply for solving problems:
+
+# 1-State the problem clearly.Identify the input & output formats.
+# 2-Come up with some examples input & outputs.Try to cover all edge cases
+# 3-Come up with a correct solution for the problem.State it in plain English
+# 4-Implement the solution and test it using examples inputs.Fix bugs,if any
+# 5-Analyze the algorithm's complexity and identify inefficiencies, if any.
+# 6-Apply the right technique to overcome the inefficiency.Repeat steps 3 to 6.
+
+# 1.State the problem clearly, Identify the input & output formats
+
+# Problem
+# We need to create a data structure which can store 100 million records and perform insertion, search, update 
+# and list operations efficiently.
+
+# input
+# the key inputs to our data structure are user profiles, which contain the username, name and email of a user
+
+
 class User:
-    def __init__(self,username, name, email):
+    def __init__(self, username, name, email):
         self.username = username
         self.name = name
         self.email = email
+    def __str__(self):
+        return f"username: {self.username}, name: {self.name} ,email: {self.email}"
+    
+
+# class UserDatabase:
+#     def insert(self, user):
+#         pass
+
+#     def find(self,username):
+#         pass
+
+#     def update(self, user):
+#         pass 
+
+#     def list_all(self):
+#         pass
+
+#VVVI
+#It's a good practice to list out the signatures of different class functions before we 
+# actually implement the class
+
+
+# 2- Come up with some example input & outputs.input
+
+aakash = User('aakash', 'Aakash Rai', 'aakash@example.com')
+biraj = User('biraj', 'Biraj Das','biraj@example.com')
+hemanth = User('hemanth', 'Hemanth Jain', 'hemanth@example.com')
+jadhesh = User('jadhesh', 'Jadhesh Verma', 'jadhesh@example.com')
+siddhant = User('siddhant', 'Siddhant Sinha','siddhant@example.com')
+sonaksh = User('sonaksh', 'Sonaksh Kumar', 'sonaksh@example.com')
+vishal = User('vishal', 'Vishal Goel','vishal@example.com')
+
+#Exercise : List some scenarios for testing the class methods insert, find, update and list_all
+# 1-Insert:
+#     A.Inserting into an empty database of users
+#     B.Trying to insert a user with a username that already exist
+#     C.Inserting a user with a username that does not exist
+
+# 3- Come up with a correct solution. State it in plain English
+
+# the various functions can be implemented as follows
+
+# 1-insert:Loop through the list and add the new user at a position that keeps the list sorted
+# 2-Find:Loop through the list and find the user object with the username matching the query
+# 3-Update:Loop through the list, find the user object matching the query and update the details.
+# 4-List :Return the list of user objects
+
+
+# 4-Implement the solution and test it using example inputs.
+
+class UserDatabase:
+    def __init__(self):
+        self.users = []
+    def insert(self, user):
+        i = 0
+        while i < len(self.users):
+            if self.users[i].username > user.username:
+                break
+            i += 1
+        self.users.insert(i, user)
+
+    def find(self, username):
+        for user in self.users:
+            if user.username == username:
+                return user
+
+    def update(self, user):
+        target = self.find(user.username)
+        target.name, target.email = user.name, user.email
+
+    def list_all(self):
+        return self.users
+    
+database = UserDatabase()
+database.insert(hemanth)
+database.insert(aakash)
+database.insert(siddhant)
+
+user = database.find('siddhant')
+print(user)
+database.update(User(username='siddhant',name='Siddhant U',email='siddhantu@example.com'))
+user = database.find('siddhant')
+print(user)
+print(database.list_all())
+
+# 5-Analyze the algorithms complexity and identify inefficiencies
+#the operations insert, find, update involves iterating over a list of users, in the worst case
+#,they may take up to N iterations to return a result, where N is the total number of users
+#list_all however, simply returns the existing internal list of users
+# thus, the time complexity of the various operations are:
+# 1.insert:O(N)
+# 2.Find:O(N)
+# 3.Update:O(N)
+# 4.List:O(1)
