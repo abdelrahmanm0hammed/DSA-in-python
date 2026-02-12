388a86303cd4d2bdfad4c87a5eda44ddbc906df8
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-12 13:10:59 +0200

    creating a function to test binary search tree

--- binary_and_binarySearch_tree.py
+++ binary_and_binarySearch_tree.py
@@ -193,3 +193,31 @@ def traversal_in_order(node):
     return (traversal_in_order(node.left) + [node.key] + 
         traversal_in_order(node.right))
 
+def tree_height(node):
+    if node is None:
+        return 0
+    return 1 + max(tree_height(node.left), tree_height(node.right))
+def tree_size(node):
+    if node is None:
+        return 0
+    return 1 +(tree_size(node.left)+tree_size(node.right))
+
+def remove_none(nums):
+    return [x for x in nums if x is not None]
+
+#Binary search tree checker function
+
+def is_bst(node):
+    if node is None:
+        return True, None, None
+    is_bst_l, min_l, max_l = is_bst(node.left)
+    is_bst_r, min_r, max_r = is_bst(node.right)
+
+    is_bst_node = (is_bst_l and is_bst_r and
+                   (min_l is None or node.key > min_l) and
+                   (min_r is None or node.key < min_r)
+                   )
+    min_key = min(remove_none([min_l, node.key, min_r]))
+    max_key = max(remove_none([max_l, node.key, max_r]))
+
+    return is_bst_node, min_key, max_key
