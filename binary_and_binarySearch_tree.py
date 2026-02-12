88ea6d8db1a2a49ee264713b1bac0a7be25c761f
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-12 16:40:49 +0200

    creating an insertion function to insert node to BST

--- binary_and_binarySearch_tree.py
+++ binary_and_binarySearch_tree.py
@@ -231,3 +231,14 @@ class BSTNode():
         self.right = None
         self.parent = None
 
+def insert(node, key, value):
+    if node is None:
+        node = BSTNode(key, value)
+    elif key < node.key:
+        node.left = insert(node.left, key, value)
+        node.left.parent = node
+    elif key > node.key:
+        node.right = insert(node.right, key, value)
+        node.right.parent = node
+    return node 
+
