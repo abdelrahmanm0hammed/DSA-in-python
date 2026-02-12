fba9f221ba7cee0d19d7963957d406e94d8af75f
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-12 17:24:37 +0200

    creating a function to test the balance of tree

--- binary_and_binarySearch_tree.py
+++ binary_and_binarySearch_tree.py
@@ -263,3 +263,12 @@ def list_all(node):
     if node is  None:
         return []
     return list_all(node.left) + [(node.key, node.value)] + list_all(node.right)
+def is_balanced(node):
+    if node is None:
+        return True, 0
+    balanced_l, height_l = is_balanced(node.left)
+    balanced_r, height_r = is_balanced(node.right)
+    balanced = balanced_l and balanced_r and abs(height_l-height_r)<=1
+    height = 1 + max(height_l, height_r)
+
+    return balanced, height
