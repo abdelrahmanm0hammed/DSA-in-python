1fc0ffc140db98ccf391bdbe380dbfbbdc524c99
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-16 22:40:25 +0200

    creating basic hash table

---
+++ Hash_table.py
@@ -0,0 +1,40 @@
+# Hash-table
+
+def get_index(data_list, a_string):
+    #variable to store the result (updated each iteration)
+    result = 0
+
+    for a_character in a_string:
+        a_number = ord(a_character)
+        result += a_number
+
+    list_index = result % len(data_list)
+    
+    return list_index
+
+
+MAX_HASH_TABLE_SIZE = 4096
+
+class BasicHashTable:
+    def __init__(self, max_size=MAX_HASH_TABLE_SIZE):
+        self.data_list = [None] * max_size
+
+    def insert(self, key, value):
+        idx = get_index(self.data_list, key)
+
+        self.data_list[idx] = key, value
+    
+    def find(self, key):
+        idx = get_index(self.data_list, key)
+        kv = self.data_list[idx]
+        if kv is None:
+            return None
+        else:
+            key, value = kv
+        
+    def update(self, key, value):
+        idx = get_index(self.data_list, key)
+        self.data_list[idx]= key, value
+    
+    def list_all(self):
+        return [kv[0] for kv in self.data_list if kv is not None]
